5b80342fa749368b8c48a9b279e039360ff4d7c0
David Bridges <david-bridges@hotmail.co.uk>
2017-10-22 04:05:15 +0100

    initial commit

--- how_to_ESM.py
+++ how_to_ESM.py
@@ -20,7 +20,6 @@ rect = cv2.rectangle(mondrian, (x_start,y_start),(x_end,y_end),(18,247,41),3,8)
 # cv2.imshow('mask',rect)
 
 # Show cropped image
-
 cropped_image = mondrian[y_start:y_end,x_start:x_end]
 # cv2.imshow('image',cropped_image)
 # cv2.waitKey(0)
@@ -32,24 +31,7 @@ hue =  cv2.calcHist([hsv], [0], None, [12], [0,180]).astype(int)
 hue_conversion = hue / hue.sum() # sum = number of pixels - creates percentage of total pixels per bin
 print('The number of pixels in the cropped image is 120*120, or {} pixels.'. format(hue.sum()))
 
-# for i in hue:
-# 	for j in i:
-# 		print(j)
-
-# plt.bar(range(0,12), hue_conversion)
-# plt.show()
-
-color = ['b','g','r']
-labels = ['h','s','v']
-hsv_range = [180,255,255]
-
 fig, ax1 = plt.subplots()
-
-# for i,col in enumerate(color):
-# 	histr=cv2.calcHist([hsv], [i], None, [12], [0,hsv_range[i]])
-# 	ax1.plot(range(1,13),histr,color=col,label=labels[i], lw = 3)
-# 	ax1.legend()
-# plt.colorbar(cmap = plt.imshow(mondrian), orientation = 'horizontal', ax=ax1)
 
 # Set x locs
 x=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
